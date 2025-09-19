d7bab4a1346b5abcea5464db752507f1655fbfc1
lijun <hlj89823@163.com>
2025-09-19 18:20:28 +0800

    langchain query

--- langchain_lian/7_langchain_sql_agent.py
+++ langchain_lian/7_langchain_sql_agent.py
@@ -1,8 +1,45 @@
 from langchain_community.utilities import SQLDatabase
+from core.deepseek_llm import deepseek_llm as llm
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+from langchain.chains import create_sql_query_chain
+from operator import itemgetter
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
 
 db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+# print(db.dialect)
+# print(db.get_usable_table_names())
+print(db.run("SELECT * FROM Artist LIMIT 10;"))
 
-# from langchain.chains import create_sql_query_chain
-#
-#
-# chain = create_sql_query_chain(SQL_PROMPT, engine)
+
+chain = create_sql_query_chain(llm, db)
+response = chain.invoke({"question": "How many employees are there"})
+print(response)
+print(db.run(response))
+
+execute_query = QuerySQLDataBaseTool(db=db)
+write_query = create_sql_query_chain(llm, db)
+chain = write_query | execute_query
+chain.invoke({"question": "How many employees are there"})
+
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, corresponding SQL query, and SQL result, answer the user question.
+
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """
+)
+
+chain = (
+    RunnablePassthrough.assign(query=write_query).assign(
+        result=itemgetter("query") | execute_query
+    )
+    | answer_prompt
+    | llm
+    | StrOutputParser()
+)
+
+result = chain.invoke({"question": "How many employees are there"})
+print(result)
